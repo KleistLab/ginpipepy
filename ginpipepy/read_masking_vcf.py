c7535360e0f72debcc3fb30dca1548feaa113671
Maria Trofimova <trofimovamw@gmail.com>
2021-07-21 16:54:39 +0200

    Update read_masking_vcf.py

--- ginpipepy/read_masking_vcf.py
+++ ginpipepy/read_masking_vcf.py
@@ -13,24 +13,24 @@ class VCFreader:
 
 	def __init__(self, filename, reffile, fp_dict):
 		'''Class: read in Variant Calling File and remove masked bases from sequence fingerprints
-        :param filename: path to Variant Calling File
-        :type filename: str
-        :param reffile: path to reference file (that VCF belongs to)
-        :type reffile: str
-        :param fp_dict: list of sequence fingerprints in format ('id','MUT_POS_1>MUT_BASE-MUT_POS_2>MUT_BASE-...')
-        :type fp_dict: list
-        '''
+        	:param filename: path to Variant Calling File
+        	:type filename: str
+        	:param reffile: path to reference file (that VCF belongs to)
+        	:type reffile: str
+        	:param fp_dict: list of sequence fingerprints in format ('id','MUT_POS_1>MUT_BASE-MUT_POS_2>MUT_BASE-...')
+        	:type fp_dict: list
+        	'''
 		self.fp_dict = fp_dict
 		self.filename = filename
 		self.reffile = reffile
 
 	def _getMaskingFromVCF(self):
 		"""
-        Gets a list of bases from a Variant Calling File that have a FILTER tag
+        	Gets a list of bases from a Variant Calling File that have a FILTER tag
 		'mask'
-        :returns: posiions of bases that should be masked
-        :rtype: list
-        """
+        	:returns: posiions of bases that should be masked
+        	:rtype: list
+        	"""
 		vcfile = vcf.Reader(open(self.filename),'r')
 		masking_list = []
 		for record in vcfile:
@@ -41,12 +41,12 @@ class VCFreader:
 
 	def maskBasesInFP(self):
 		"""
-        Removes mutated poositions from fingerprints based on Variant Calling
+        	Removes mutated poositions from fingerprints based on Variant Calling
 		File, where FILTER tag has value 'mask' and returns a filtered fingerprint
 		list
-        :returns: set of filtered fingerprints in format (id_string,fp_string)
-        :rtype: list
-        """
+        	:returns: set of filtered fingerprints in format (id_string,fp_string)
+        	:rtype: list
+        	"""
 		masking_list = self._getMaskingFromVCF()
 		newSeqSets = []
 		for t, seqSet in enumerate(self.fp_dict):
